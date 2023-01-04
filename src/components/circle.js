66221cad6ecb40ec218fee2d2ade94f4bdda74ee
Vishal Vaish <114350565+vishal-vaish@users.noreply.github.com>
2023-01-04 10:42:56 +0300

    first

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,12 @@
 // import using require
-
+var Shape=require('./shape.js');
 // declare class
-
+class Circle extends Shape{
+    constructor(color){
+        super();
+    }
+    calculateArea();
+}
 
 // export class using module.exports
+module.exports=Circle;
